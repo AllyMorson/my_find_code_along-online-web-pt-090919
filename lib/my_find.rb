8fa8755c682788b211e86cc604ab038aa8be3cb1
Ally Morson <ally.morson@gmail.com>
2019-10-04 02:16:51 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,8 +3,8 @@ require 'pry'
 def my_find(collection)
 i = 0
 while i < collection.length
-yield(collection[i])
 return collection[i] #If the block returns true, return the argument we passed into yield AKA the element in the array that evaluated to true. This will exit the loop and the element will be the return value of the method.
+if yield(collection[i])
 end
 i = i + 1
 end
