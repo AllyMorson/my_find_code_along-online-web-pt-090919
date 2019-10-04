7075cdad210009d2f88fc01bc10cd99eea021f0f
Ally Morson <ally.morson@gmail.com>
2019-10-04 02:15:40 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,5 +5,7 @@ i = 0
 while i < collection.length
 yield(collection[i])
 return collection[i] #If the block returns true, return the argument we passed into yield AKA the element in the array that evaluated to true. This will exit the loop and the element will be the return value of the method.
+end
 i = i + 1
+end
 end
